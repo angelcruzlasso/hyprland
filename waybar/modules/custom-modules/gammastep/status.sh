c7ae20b4c31c6c4d4d7c0148ef8d73f3422c7e0a
angelcruzlasso <angelcruzlasso@gmail.com>
2025-05-14 04:16:12 -0500

    cambios adicionales...

--- waybar/modules/custom-modules/gammastep/status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if pgrep  gammastep >/dev/null; then 
-    printf '{"text":"󱩌","class":"enabled"}'; 
-else
-    printf '{"text":"󱩌"}';
-fi
--- waybar/modules/custom-modules/menu-help/rofi-help.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rofi -show fb -modes "fb:~/.config/waybar/modules/custom-modules/menu-help/test.sh" -theme ~/.local/share/rofi/themes/spotlight-dark.rasi
